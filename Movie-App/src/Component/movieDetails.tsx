6da404d24732d011fcdf5810bdccbef90381b611
Nitin-cyberpunk <122776086+Nitin-cyperpunk@users.noreply.github.com>
2025-09-14 21:19:46 +0530

    second commit

---
+++ Movie-App/src/Component/movieDetails.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function movieDetails() {
+  return (
+    <>
+    <div className="text-white text-center text-6xl">movieDetails</div>
+    </>
+  )
+}
+
+export default movieDetails
